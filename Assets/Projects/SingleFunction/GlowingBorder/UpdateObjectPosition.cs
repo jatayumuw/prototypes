84cad1d732b1390460dd298672dcc736e98dab0f
jatayumuw <jatayumuw@gmail.com>
2024-06-18 09:55:09 +0700

    edit glowing border

--- Assets/Projects/SingleFunction/GlowingBorder/UpdateObjectPosition.cs
+++ Assets/Projects/SingleFunction/GlowingBorder/UpdateObjectPosition.cs
@@ -6,13 +6,19 @@ public class UpdateObjectPosition : MonoBehaviour
 {
     public Material material;
     public Transform targetObject;
+    public float minDistance = 2f;
     public float maxDistance = 10f;
+    public float minAlpha = 0f;
+    public float maxAlpha = 1f;
 
     void Start()
     {
         if (material != null)
         {
+            material.SetFloat("_MinDistance", minDistance);
             material.SetFloat("_MaxDistance", maxDistance);
+            material.SetFloat("_MinAlpha", minAlpha);
+            material.SetFloat("_MaxAlpha", maxAlpha);
         }
     }
 
@@ -24,4 +30,3 @@ public class UpdateObjectPosition : MonoBehaviour
         }
     }
 }
-
